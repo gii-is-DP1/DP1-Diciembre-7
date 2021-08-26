2ba6393c6493dabe940a286cc2a5dd092dfa578f
SIAMION DANKO <siadan@alum.us.es>
2021-08-26 18:10:48 +0200

    Creados los modelos de Oficina, TipoVehiculo y Vehiculo. Falta la relacion de Vehiculo con Reserva.

---
+++ src/main/java/org/springframework/samples/petclinic/model/Oficina.java
@@ -0,0 +1,83 @@
+package org.springframework.samples.petclinic.model;
+
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.persistence.JoinColumn;
+
+@Entity
+@Table(name="oficinas")
+public class Oficina extends BaseEntity{
+	
+	@Column(name="ciudad")
+	@NotBlank
+	private String ciudad;
+	
+	@Column(name="direccion")
+	@NotBlank
+	private String direccion;
+	
+	@Column(name="codigoPostal")
+	@NotBlank
+	private Integer codigoPostal;
+	
+	@ManyToMany(cascade = CascadeType.ALL)
+	@JoinTable(name="vehiculosOficinas",
+	joinColumns = @JoinColumn(name = "oficina_id"),
+	inverseJoinColumns = @JoinColumn(name = "vehiculo_id"))
+	private Set<Vehiculo> vehiculos;
+	
+	@ManyToOne
+	@JoinColumn(name="empresa_id")
+	private Empresa empresa;
+
+	public String getCiudad() {
+		return ciudad;
+	}
+
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public Integer getCodigoPostal() {
+		return codigoPostal;
+	}
+
+	public void setCodigoPostal(Integer codigoPostal) {
+		this.codigoPostal = codigoPostal;
+	}
+
+	public Set<Vehiculo> getVehiculos() {
+		return vehiculos;
+	}
+
+	public void setVehiculos(Set<Vehiculo> vehiculos) {
+		this.vehiculos = vehiculos;
+	}
+
+	public Empresa getEmpresa() {
+		return empresa;
+	}
+
+	public void setEmpresa(Empresa empresa) {
+		this.empresa = empresa;
+	}
+	
+	
+
+}
