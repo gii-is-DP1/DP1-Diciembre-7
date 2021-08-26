fd4f8a9da8bbf858f1f64479601248588eced504
SIAMION DANKO <siadan@alum.us.es>
2021-08-26 18:54:27 +0200

    Creado modelo Reserva
    +Relacion Reserva-Vehiculo y Reserva-Conductor

---
+++ src/main/java/org/springframework/samples/petclinic/model/Reserva.java
@@ -0,0 +1,93 @@
+package org.springframework.samples.petclinic.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name="reservas")
+public class Reserva extends BaseEntity{
+	
+	@Column(name="fechaInicio")
+	@NotBlank
+	private LocalDate fechaInicio;
+	
+	@Column(name="fechaFin")
+	@NotBlank
+	private LocalDate fechaFin;
+	
+	@Column(name="precioFinal")
+	@NotBlank
+	@Min(0)
+	private Double precioFinal;
+	
+	@ManyToOne
+	@JoinColumn(name = "actor_id")
+	private Cliente cliente;
+	
+	@OneToOne
+	@JoinColumn(name="vehiculo_id", referencedColumnName = "vehiculo_id")
+	private Vehiculo vehiculo;
+	
+	@ManyToOne
+	@JoinColumn(name="actor_id")
+	private Conductor conductor;
+
+	public LocalDate getFechaInicio() {
+		return fechaInicio;
+	}
+
+	public void setFechaInicio(LocalDate fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+
+	public LocalDate getFechaFin() {
+		return fechaFin;
+	}
+
+	public void setFechaFin(LocalDate fechaFin) {
+		this.fechaFin = fechaFin;
+	}
+
+	public Double getPrecioFinal() {
+		return precioFinal;
+	}
+
+	public void setPrecioFinal(Double precioFinal) {
+		this.precioFinal = precioFinal;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Vehiculo getVehiculo() {
+		return vehiculo;
+	}
+
+	public void setVehiculo(Vehiculo vehiculo) {
+		this.vehiculo = vehiculo;
+	}
+
+	public Conductor getConductor() {
+		return conductor;
+	}
+
+	public void setConductor(Conductor conductor) {
+		this.conductor = conductor;
+	}
+	
+	
+
+}
