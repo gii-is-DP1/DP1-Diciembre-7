6c63d1bbb594740e2f0978b0ca52627f44288cf5
Javier Pérez <javpermar4@alum.us.es>
2021-08-27 19:27:12 +0200

    Updated ConductorService

--- src/main/java/org/springframework/samples/petclinic/service/ConductorService.java
+++ src/main/java/org/springframework/samples/petclinic/service/ConductorService.java
@@ -1,18 +1,25 @@
 package org.springframework.samples.petclinic.service;
 
+import java.time.LocalDate;
 import java.util.Collection;
+import java.util.HashSet;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.samples.petclinic.model.Conductor;
+import org.springframework.samples.petclinic.model.Reserva;
 import org.springframework.samples.petclinic.model.TipoVehiculo;
 import org.springframework.samples.petclinic.repository.ConductorRepository;
+import org.springframework.samples.petclinic.repository.ReservaRepository;
 import org.springframework.samples.petclinic.repository.VehiculoRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 public class ConductorService {
 	
 	private ConductorRepository conductorRepository;
+	
+	@Autowired
+	private ReservaRepository reservaRepository;
 	
 	@Autowired
 	private VehiculoRepository vehiculoRepository;
@@ -34,16 +41,33 @@ public class ConductorService {
 	}
 	
 	@Transactional(readOnly = true)
-	public Collection<Conductor> findConductoresPorCiudadYPermiso(String ciudad, String tipoVehiculo) throws DataAccessException{
+	public Collection<Conductor> findConductoresPorCiudadPermiso(String ciudad, TipoVehiculo tipoVehiculo, LocalDate fechaInicio, LocalDate fechaFinal) throws DataAccessException{
 		Collection<Conductor> conductoresCiudad = conductorRepository.findConductoresPorCiudad(ciudad);
-		Collection<Conductor> conductoresCiudadYPermiso;
-		Collection<TipoVehiculo> tiposVehiculos = vehiculoRepository.
+		Collection<Conductor> conductoresCiudadYPermiso = new HashSet<Conductor>();
+		Collection<TipoVehiculo> tiposVehiculos = vehiculoRepository.findTipoVehiculo();
+		Collection<Conductor> conductoresCiudadPermisoYFecha = new HashSet<Conductor>();
 		for(Conductor c:conductoresCiudad) {
-			if(tipoVehiculo) {
+			if(tiposVehiculos.contains(tipoVehiculo) && c.getPermisoCoche()==true &&(tipoVehiculo.getName().equals("SUV") || tipoVehiculo.getName().equals("Turismo") || tipoVehiculo.getName().equals("Todoterreno") || tipoVehiculo.getName().equals("Furgoneta")) ) {
+				conductoresCiudadYPermiso.add(c);
+			}else if(tiposVehiculos.contains(tipoVehiculo) && c.getPermisoBarco()==true && (tipoVehiculo.getName().equals("Lancha") || tipoVehiculo.getName().equals("Velero") || tipoVehiculo.getName().equals("Yate") || tipoVehiculo.getName().equals("motoAgua"))){
+				conductoresCiudadYPermiso.add(c);
+			}
+		}
+		for(Conductor c: conductoresCiudadYPermiso) {
+			Collection<Reserva> reservasConductor = reservaRepository.findReservasByConductor(c);
+			for(Reserva r: reservasConductor) {
+				if(r.getFechaInicio().) {
+					
+				}
 				
 			}
-		}	
-		return conductoresCiudadYPermiso;				
+			if() {
+				)
+			}else {
+				conductoresCiudadPermisoYFecha.add(c);
+			}
+		}
+		return conductoresCiudadPermisoYFecha;				
 	}
 	
 	@Transactional
