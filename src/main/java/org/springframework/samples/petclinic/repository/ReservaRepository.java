cd667fe73d5bd6d9504196a3a10d53ca8f5f9770
SIAMION DANKO <siadan@alum.us.es>
2021-08-27 19:26:45 +0200

    Querys para conductor y primera version final del servicio de vehiculo

--- src/main/java/org/springframework/samples/petclinic/repository/ReservaRepository.java
+++ src/main/java/org/springframework/samples/petclinic/repository/ReservaRepository.java
@@ -1,8 +1,17 @@
 package org.springframework.samples.petclinic.repository;
 
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.samples.petclinic.model.Conductor;
 import org.springframework.samples.petclinic.model.Reserva;
 
 public interface ReservaRepository extends CrudRepository<Reserva, Integer>{
+	
+	//Reservas por conductor
+	@Query("SELECT reserva FROM Reserva reserva WHERE reserva.conductor=:conductor")
+	public Collection<Reserva> findReservaByConductor(@Param("conductor") Conductor conductor);
 
 }
