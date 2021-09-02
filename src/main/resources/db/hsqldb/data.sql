29db956b6bfc2a5279f178753cd0e4cb0d3d70f7
Javier Pérez <javpermar4@alum.us.es>
2021-09-02 13:28:45 +0200

    Base de datos poblada con datos de uso

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -7,6 +7,15 @@ INSERT INTO authorities(id,username,authority) VALUES (2,'owner1','owner');
 -- One vet user, named vet1 with passwor v3t
 INSERT INTO users(username,password,enabled) VALUES ('vet1','v3t',TRUE);
 INSERT INTO authorities(id,username,authority) VALUES (3,'vet1','veterinarian');
+-- One client user, named cliente1 with password cliente1
+INSERT INTO users(username,password,enabled) VALUES('cliente1','cliente1',TRUE);
+INSERT INTO authorities(id,username,authority) VALUES (4, 'cliente1', 'cliente');
+-- One empresa user, named conductor1 with password conductor1
+INSERT INTO users(username,password,enabled) VALUES('empresa1','empresa1',TRUE);
+INSERT INTO authorities(id,username,authority) VALUES (5, 'empresa1', 'empresa');
+-- One conductor user, named conductor1 with password conductor1
+INSERT INTO users(username,password,enabled) VALUES('conductor1','conductor1',TRUE);
+INSERT INTO authorities(id,username,authority) VALUES (6, 'conductor1', 'conductor');
 
 INSERT INTO vets VALUES (1, 'James', 'Carter');
 INSERT INTO vets VALUES (2, 'Helen', 'Leary');
@@ -25,12 +34,60 @@ INSERT INTO vet_specialties VALUES (3, 3);
 INSERT INTO vet_specialties VALUES (4, 2);
 INSERT INTO vet_specialties VALUES (5, 1);
 
+INSERT INTO tipos_vehiculo VALUES (1, 'SUV');
+INSERT INTO tipos_vehiculo VALUES (2, 'Turismo');
+INSERT INTO tipos_vehiculo VALUES (3, 'Todoterreno');
+INSERT INTO tipos_vehiculo VALUES (4, 'Furgoneta');
+INSERT INTO tipos_vehiculo VALUES (5, 'Lancha');
+INSERT INTO tipos_vehiculo VALUES (6, 'Velero');
+INSERT INTO tipos_vehiculo VALUES (7, 'Yate');
+INSERT INTO tipos_vehiculo VALUES (8, 'Moto de agua');
+
 INSERT INTO types VALUES (1, 'cat');
 INSERT INTO types VALUES (2, 'dog');
 INSERT INTO types VALUES (3, 'lizard');
 INSERT INTO types VALUES (4, 'snake');
 INSERT INTO types VALUES (5, 'bird');
 INSERT INTO types VALUES (6, 'hamster');
+
+INSERT INTO cliente VALUES (1, 'example@example.com','Javier','665738445','Avenida Reina Mercedes s/n','17473663B','cliente1');
+INSERT INTO cliente VALUES (2, 'simeon@simeon.com','Simeon','123456789','Avenida Reina Mercedes s/n','12641431V','cliente1');
+
+INSERT INTO empresa VALUES (1, 'example@example.com','Universidad de Sevilla','665738445','España','empresa1');
+INSERT INTO empresa VALUES (2, 'ejemplo@ejemplo.com','Aytos','123456789','España','empresa1');
+
+INSERT INTO conductor VALUES(1, 'simeon@simeon.com','Simeon','123456789','Sevilla','12641431V','7',TRUE,FALSE,150.0,12.0,'conductor1');
+INSERT INTO conductor VALUES(2, 'example@example.com','Javier','665738445','Sevilla','17473663B','9',TRUE,TRUE,180.0,20.0,'conductor1');
+
+INSERT INTO oficinas(id,ciudad,codigo_postal,direccion,empresa_id) VALUES(1,'Sevilla', 41400,'Avenida Reina Mercedes s/n', 1);
+INSERT INTO oficinas(id,ciudad,codigo_postal,direccion,empresa_id) VALUES(2,'Madrid', 41400,'Plaza del Sol', 1);
+INSERT INTO oficinas(id,ciudad,codigo_postal,direccion,empresa_id) VALUES(3,'Sevilla', 41400,'Plaza de España', 1);
+INSERT INTO oficinas(id,ciudad,codigo_postal,direccion,empresa_id) VALUES(4,'Malaga', 41400,'Puerto deportivo', 2);
+
+INSERT INTO vehiculos(id,marca,modelo,precio_base,precio_por_dia,stock,tipo_vehiculo_id) VALUES (1,'Seat','Leon',150.0,15.0,50,2);
+INSERT INTO vehiculos(id,marca,modelo,precio_base,precio_por_dia,stock,tipo_vehiculo_id) VALUES (2,'Seat','Ibiza',120.0,12.0,80,2);
+INSERT INTO vehiculos(id,marca,modelo,precio_base,precio_por_dia,stock,tipo_vehiculo_id) VALUES (3,'Land Rover','Range Rover',150.0,15.0,20,3);
+INSERT INTO vehiculos(id,marca,modelo,precio_base,precio_por_dia,stock,tipo_vehiculo_id) VALUES (4,'Quicksilver','Velero deportivo',350.0,25.0,10,6);
+INSERT INTO vehiculos(id,marca,modelo,precio_base,precio_por_dia,stock,tipo_vehiculo_id) VALUES (5,'Renault','Clio',150.0,15.0,80,2);
+INSERT INTO vehiculos(id,marca,modelo,precio_base,precio_por_dia,stock,tipo_vehiculo_id) VALUES (6,'Renault','Megane',250.0,15.0,40,2);
+INSERT INTO vehiculos(id,marca,modelo,precio_base,precio_por_dia,stock,tipo_vehiculo_id) VALUES (7,'Ford','Focus',150.0,15.0,50,2);
+INSERT INTO vehiculos(id,marca,modelo,precio_base,precio_por_dia,stock,tipo_vehiculo_id) VALUES (8,'Dacia','Sandero',220.0,20.0,10,2);
+INSERT INTO vehiculos(id,marca,modelo,precio_base,precio_por_dia,stock,tipo_vehiculo_id) VALUES (9,'Nissan','Qashqai',250.0,22.0,35,2);
+
+INSERT INTO vehiculos_oficinas(oficina_id,vehiculo_id) VALUES (1,1);
+INSERT INTO vehiculos_oficinas(oficina_id,vehiculo_id) VALUES (1,2);
+INSERT INTO vehiculos_oficinas(oficina_id,vehiculo_id) VALUES (1,3);
+INSERT INTO vehiculos_oficinas(oficina_id,vehiculo_id) VALUES (4,4);
+INSERT INTO vehiculos_oficinas(oficina_id,vehiculo_id) VALUES (2,5);
+INSERT INTO vehiculos_oficinas(oficina_id,vehiculo_id) VALUES (2,1);
+INSERT INTO vehiculos_oficinas(oficina_id,vehiculo_id) VALUES (3,8);
+INSERT INTO vehiculos_oficinas(oficina_id,vehiculo_id) VALUES (1,9);
+INSERT INTO vehiculos_oficinas(oficina_id,vehiculo_id) VALUES (1,7);
+INSERT INTO vehiculos_oficinas(oficina_id,vehiculo_id) VALUES (3,6);
+INSERT INTO vehiculos_oficinas(oficina_id,vehiculo_id) VALUES (3,9);
+
+
+
 
 INSERT INTO owners VALUES (1, 'George', 'Franklin', '110 W. Liberty St.', 'Madison', '6085551023', 'owner1');
 INSERT INTO owners VALUES (2, 'Betty', 'Davis', '638 Cardinal Ave.', 'Sun Prairie', '6085551749', 'owner1');
